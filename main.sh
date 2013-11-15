5d27a19fea881e9589ca234aaa2fd7f8b2a4ce40
Robert Parrott <parrott@zephyr.local>
2013-11-15 18:35:14 -0500

    update images to use.

--- main.sh
+++ main.sh
@@ -12,11 +12,12 @@ fi
 source ./bin/setup-admin-resources.sh
 source ./bin/test-setup.sh
 
-IMAGES="http://download.cirros-cloud.net/0.3.1/cirros-0.3.1-x86_64-uec.tar.gz \
-        http://download.cirros-cloud.net/0.3.1/cirros-0.3.1-x86_64-disk.img \
-        http://smoser.brickies.net/ubuntu/ttylinux-uec/ttylinux-uec-amd64-11.2_2.6.35-15_1.tar.gz \
-        http://cloud-images.ubuntu.com/quantal/current/quantal-server-cloudimg-amd64-disk1.img \
-        http://mattdm.fedorapeople.org/cloud-images/Fedora18-Cloud-x86_64-latest.qcow2"
+IMAGES="http://download.cirros-cloud.net/0.3.1/cirros-0.3.1-x86_64-disk.img \
+        http://cloud-images.ubuntu.com/raring/20131008/raring-server-cloudimg-amd64-disk1.img \
+        http://download.fedoraproject.org/pub/fedora/linux/releases/19/Images/x86_64/Fedora-x86_64-19-20130627-sda.qcow2 \
+        http://knoppix.hostingxtreme.com/KNOPPIX_V7.2.0CD-2013-06-16-EN.iso \
+        http://softlayer-dal.dl.sourceforge.net/project/nst/NST/NST%2018-4509/nst-18-4509.i686.iso "
+
     
 for IMAGE in ${IMAGES}; do 
 	./bin/upload-image.sh ${IMAGE}
