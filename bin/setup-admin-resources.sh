a3cb93da55ffba86b981856a41200ddade06b914
Rob Parrott <robparrott@gmail.com>
2013-12-06 18:15:49 -0500

    check for object before creating them

--- bin/setup-admin-resources.sh
+++ bin/setup-admin-resources.sh
@@ -32,25 +32,55 @@ L3_AGENT_ID=$( $QUANTUM agent-list | grep "L3 agent" | awk '{print $2}' )
 # -----------------------------
 
 # Create a private network
-$QUANTUM net-create admin-private-net 
+tenant="admin"
+tenant_id=$( get_k_id tenant " ${tenant} " )
 
-# Create a couple subnets
+# Create a private net and subnet
+net_id=$( get_q_id net "admin_private_net" )
+if [ -z ${net_id} ]; then
+    $QUANTUM net-create    --tenant_id $tenant_id "admin_private_net"
+    net_id=$( get_q_id net "admin_private_net" )
+fi
 
-$QUANTUM subnet-create --name admin-private-subnet  admin-private-net 10.12.34.0/24
-$QUANTUM subnet-create --name admin-private-subnet2 admin-private-net 10.56.78.0/24
+
+# Create a subnet or two
+cidr="10.12.34.0/24"
+subnet_id=$( get_q_id subnet $cidr )
+if [ -z ${subnet_id} ]; then
+    $QUANTUM subnet-create --tenant_id $tenant_id \
+                          --ip_version 4 \
+                          --name "admin_private_subnet"  \
+                           "admin_private_net" "$cidr"
+    subnet_id=$( get_q_id subnet "$cidr" )
+fi
+
+cidr="10.56.78.0/24"
+subnet2_id=$( get_q_id subnet $cidr )
+if [ -z ${subnet2_id} ]; then
+    $QUANTUM subnet-create --tenant_id $tenant_id \
+                          --ip_version 4 \
+                          --name "admin_private_subnet2"  \
+                           "admin_private_net" "$cidr"
+    subnet2_id=$( get_q_id subnet "$cidr" )
+fi
 
 # Create a router & add it to the L3 agent
+router_id=$(  get_q_id router "admin_router" )
+if [ -z ${router_id} ]; then
+    $QUANTUM router-create --tenant_id $tenant_id "admin_router"
+    router_id=$(  get_q_id router "admin_router" )
+fi
+$QUANTUM router-interface-add "admin_router" $subnet_id || /bin/true
 
-$QUANTUM router-create admin-router
-$QUANTUM l3-agent-router-add ${L3_AGENT_ID} admin-router
+$QUANTUM l3-agent-router-add ${L3_AGENT_ID} admin_router
 
 # connect it to a couple subnets
 
 PROJECT_SUBNET_ID=$( get_q_id subnet 10.12.34.0/24 )
 PROJECT_SUBNET_ID2=$( get_q_id subnet 10.56.78.0/24 )
 
-$QUANTUM router-interface-add admin-router ${PROJECT_SUBNET_ID}
-$QUANTUM router-interface-add admin-router ${PROJECT_SUBNET_ID2}
+#$QUANTUM router-interface-add admin-router ${PROJECT_SUBNET_ID}
+#$QUANTUM router-interface-add admin-router ${PROJECT_SUBNET_ID2}
 
 
 #------------------------------
